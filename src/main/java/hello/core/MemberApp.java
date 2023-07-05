24b6afcc891f405dd7421563aa0152875db78643
Youngkwang Lim <richard08@naver.com>
2023-07-05 20:33:13 +0900

    first commit

---
+++ src/main/java/hello/core/MemberApp.java
@@ -0,0 +1,17 @@
+package hello.core;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+
+public class MemberApp {
+
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        Member member = new Member(1L, "member", Grade.VIP);
+        memberService.join(member);
+
+        memberService.findMember(1L);
+    }
+}
