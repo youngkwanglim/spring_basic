e53b46aa60a6b25167ed84323efd077fb9a60701
Youngkwang Lim <richard08@naver.com>
2023-07-06 22:56:19 +0900

    first commit

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -9,9 +9,11 @@ public class MemberApp {
 
     public static void main(String[] args) {
         MemberService memberService = new MemberServiceImpl();
-        Member member = new Member(1L, "member", Grade.VIP);
+        Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
-        memberService.findMember(1L);
+        Member findMember = memberService.findMember(1L);
+        System.out.println("member = " + member.getName());
+        System.out.println("findMember = " + findMember.getName());
     }
 }
