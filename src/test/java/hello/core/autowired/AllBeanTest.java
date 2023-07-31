4d306578bb110ced0f2c978008b4dfd4df82764d
Youngkwang Lim <richard08@naver.com>
2023-07-31 23:29:58 +0900

    섹션 7. 의존관계 자동 주입

--- src/test/java/hello/core/autowired/AllBeanTest.java
+++ src/test/java/hello/core/autowired/AllBeanTest.java
@@ -34,7 +34,7 @@ public class AllBeanTest {
 
     static class DiscountService {
         private final Map<String, DiscountPolicy> policyMap;
-        private final List<DiscountPolicy> policies;;;
+        private final List<DiscountPolicy> policies;;
 
         @Autowired
         public DiscountService(Map<String, DiscountPolicy> policyMap, List<DiscountPolicy> policies) {
